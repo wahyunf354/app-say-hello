52772dd327409723a5c0302ebbfcb5a7cf47b17b
wahyunf354 <wahyunurfadillah313@gmail.com>
2023-05-07 18:47:40 +0700

    menggunakan module lain

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wahyunf354/app-say-hello
+
+go 1.19
+
+require github.com/wahyunf354/go-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/wahyunf354/go-say-hello"
+)
+
+func main() {
+
+	fmt.Println(go_say_hello.SayHello())
+}
